6c8b8892ccae0beac94b63892282b79ac7f03101
mugambi645 <mugambi645@gmail.com>
2021-03-10 20:29:18 +0300

    unit tests

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
 # Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index_page(self):
+        """check if index page loads properly"""
+        response = self.client.get('your_server_ip:8000')
+        self.assertEqual(response.status_code, 404)
